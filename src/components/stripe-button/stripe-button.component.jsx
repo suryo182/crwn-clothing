4f94cfec3b22580482d08c235893946d5fbac205
suryo182 <suryo182@gmail.com>
2020-12-27 11:58:06 +0700

    updating stripe intregation and update favicon and title

---
+++ src/components/stripe-button/stripe-button.component.jsx
@@ -0,0 +1,30 @@
+import React from 'react';
+import StripeCheckout from 'react-stripe-checkout';
+
+const StripeCheckoutButton = ({ price }) => {
+  const priceForStripe = price * 100;
+  const publishableKey =
+    'pk_test_51I2nwMFQ9SPK5bh11F4IagUynMsJCD9RlRPPwCJNm6mAvFS0MO09cS6lbVpMM2yM6jW5T8PNh5nCYW8NfC60M8jf009E3MkWWu';
+
+  const onToken = (token) => {
+    console.log(token);
+    alert('Payment Successful');
+  };
+
+  return (
+    <StripeCheckout
+      label="Pay Now"
+      name="CRWN Clothing Ltd."
+      billingAddress
+      shippingAddress
+      image="https://svgshare.com/i/CUz.svg"
+      description={`Your total is ${price}`}
+      amount={priceForStripe}
+      panelLabel="Pay Now"
+      token={onToken}
+      stripeKey={publishableKey}
+    />
+  );
+};
+
+export default StripeCheckoutButton;
